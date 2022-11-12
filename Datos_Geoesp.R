cfe9377dbda0662fd81d804763e5581f79616148
sdpinilla18 <sergiodavidpp@gmail.com>
2022-11-12 02:59:56 -0500

    GeoData

--- Datos_Geoesp.R
+++ Datos_Geoesp.R
@@ -31,7 +31,293 @@ geoprop1 <- st_as_sf(x=geoprop,coords=c("lon","lat"),crs=4326) # Datos geoespaci
 
 leaflet() %>% addTiles() %>% addCircleMarkers(data=geoprop1)
 
-geoprop2 <- st_buffer(x=geoprop1,dist=2000)
+#geoprop2 <- st_buffer(x=geoprop1,dist=2000)
+
+#leaflet() %>% addTiles() %>% addCircleMarkers(data=geoprop2)
+
+## 2. Poligonos de los municipios
+
+Bogotá = getbb(place_name = "Bogotá Distrito Capital - Municipio",
+               featuretype = "boundary:administrative",
+               format_out = "sf_polygon")
+
+leaflet() %>% addTiles() %>% addPolygons(data=Bogotá)
+
+
+Medellín = getbb(place_name = "Medellín Colombia",
+                 featuretype = "boundary:administrative",
+                 format_out = "sf_polygon")
+
+Medellín <- Medellín[1,1]
+
+leaflet() %>% addTiles() %>% addPolygons(data=Medellín)
+
+Cali = getbb(place_name = "Cali Colombia",
+                 featuretype = "boundary:administrative",
+                 format_out = "sf_polygon")
+
+leaflet() %>% addTiles() %>% addPolygons(data=Cali)
+
+
+## 3. Datos OSM por hogar
+
+
+# 3.1 Centroids
+
+city <- c("Bog","Med","Cal")
+cent_lat <- c(4.6534649, 6.2443382,3.4517923)
+cent_lon <- c(-74.0836453,-75.5735530, -76.5324943)
+
+centdf <- data.frame(city,cent_lat,cent_lon)
+
+centgeo <- st_as_sf(x=centdf,coords=c("cent_lon","cent_lat"),crs=4326) # Datos geoespaciales
+
+leaflet() %>% addTiles() %>% addCircleMarkers(data=centgeo)
+
+cent <- list()
+
+cent[[1]] <- filter(centgeo,city=="Bog")
+cent[[2]] <- filter(centgeo,city=="Med")
+cent[[3]] <- filter(centgeo,city=="Cal")
+
+# 3.2 Manzanas
+
+mnz <- st_read("MNZ/MGN_URB_MANZANA.shp")
+
+mnz <- st_transform(mnz,crs=4326)
+
+mnz <- filter(mnz, COD_MPIO %in% c("05001","11001","76001") )
+
+geoprop1 <- st_join(geoprop1,mnz)
+
+mnz_data <- readRDS("mnzdata_censo_2018.Rds") 
+
+geoprop1 <- left_join(geoprop1,mnz_data,by=c("COD_DANE"="COD_DANE_ANM"))
+
+Bogota_DC = filter(geoprop1, city %in% c("Bogotá D.C"))
+
+Medellin_D = filter(geoprop1, city %in% c("Medellín"))
+
+Cali_D = filter(geoprop1, city %in% c("Cali"))
+
+# 3.3 Distancias
+
+# 3.3.1 Centroids
+cent_bog <- filter(centgeo,city=="Bog")
+Bogota_DC$dist_cent <- st_distance(x=Bogota_DC,y=cent_bog)
+
+cent_med <- filter(centgeo,city=="Med")
+Medellin_D$dist_cent <- st_distance(x=Medellin_D,y=cent_med)
+
+cent_cal <- filter(centgeo,city=="Cal")
+Cali_D$dist_cent <- st_distance(x=Cali_D,y=cent_cal)
+
+# 3.3.2 Airports
+
+air <- opq(bbox = getbb("Bogotá Distrito Capital - Municipio")) %>%
+  add_osm_feature(key = "aeroway", value = "aerodrome") %>%
+  osmdata_sf() %>% .$osm_points
+mtxar <- st_distance(x=Bogota_DC,y=air)
+Bogota_DC$dist_air <- apply(mtxar,1,min)
+
+air <- opq(bbox = getbb("Medellín Colombia")) %>%
+  add_osm_feature(key = "aeroway", value = "aerodrome") %>%
+  osmdata_sf() %>% .$osm_points
+mtxar <- st_distance(x=Medellin_D,y=air)
+Medellin_D$dist_air <- apply(mtxar,1,min)
+
+air <- opq(bbox = getbb("Cali Colombia")) %>%
+  add_osm_feature(key = "aeroway", value = "aerodrome") %>%
+  osmdata_sf() %>% .$osm_points
+mtxar <- st_distance(x=Cali_D,y=air)
+Cali_D$dist_air <- apply(mtxar,1,min)
+
+# 3.3.3 Bus stations
+
+bus <- opq(bbox = getbb("Bogotá Distrito Capital - Municipio")) %>%
+  add_osm_feature(key = "amenity", value = "bus_station") %>%
+  osmdata_sf() %>% .$osm_points
+mtxbs <- st_distance(x=Bogota_DC,y=bus)
+Bogota_DC$dist_bus <- apply(mtxbs,1,min)
+
+bus <- opq(bbox = getbb("Medellín Colombia")) %>%
+  add_osm_feature(key = "amenity", value = "bus_station") %>%
+  osmdata_sf() %>% .$osm_points
+mtxbs <- st_distance(x=Medellin_D,y=bus)
+Medellin_D$dist_bus <- apply(mtxbs,1,min)
+
+
+bus <- opq(bbox = getbb("Cali Colombia")) %>%
+  add_osm_feature(key = "amenity", value = "bus_station") %>%
+  osmdata_sf() %>% .$osm_points
+mtxbs <- st_distance(x=Cali_D,y=bus)
+Cali_D$dist_bus <- apply(mtxbs,1,min)
+
+# 3.3.4 Hospital
+
+hosp <- opq(bbox = getbb("Bogotá Distrito Capital - Municipio")) %>%
+  add_osm_feature(key = "amenity", value = "hospital") %>%
+  osmdata_sf() %>% .$osm_points
+mtxhs <- st_distance(x=Bogota_DC,y=hosp)
+Bogota_DC$dist_hosp <- apply(mtxhs,1,min)
+
+hosp <- opq(bbox = getbb("Medellín Colombia")) %>%
+  add_osm_feature(key = "amenity", value = "hospital") %>%
+  osmdata_sf() %>% .$osm_points
+mtxhs <- st_distance(x=Medellin_D,y=hosp)
+Medellin_D$dist_hosp <- apply(mtxhs,1,min)
+
+hosp <- opq(bbox = getbb("Cali Colombia")) %>%
+  add_osm_feature(key = "amenity", value = "hospital") %>%
+  osmdata_sf() %>% .$osm_points
+mtxhs <- st_distance(x=Cali_D,y=hosp)
+Cali_D$dist_hosp <- apply(mtxhs,1,min)
+
+# 3.3.5 Policia
+
+pol <- opq(bbox = getbb("Bogotá Distrito Capital - Municipio")) %>%
+  add_osm_feature(key = "amenity", value = "police") %>%
+  osmdata_sf() %>% .$osm_points
+mtxpl <- st_distance(x=Bogota_DC,y=pol)
+Bogota_DC$dist_pol <- apply(mtxpl,1,min)
+
+pol <- opq(bbox = getbb("Medellín Colombia")) %>%
+  add_osm_feature(key = "amenity", value = "police") %>%
+  osmdata_sf() %>% .$osm_points
+mtxpl <- st_distance(x=Medellin_D,y=pol)
+Medellin_D$dist_pol <- apply(mtxpl,1,min)
+
+pol <- opq(bbox = getbb("Cali Colombia")) %>%
+  add_osm_feature(key = "amenity", value = "police") %>%
+  osmdata_sf() %>% .$osm_points
+mtxpl <- st_distance(x=Cali_D,y=pol)
+Cali_D$dist_pol <- apply(mtxpl,1,min)
+
+# 3.3.6 Shops
+
+shop1 <- opq(bbox = getbb("Bogotá Distrito Capital - Municipio")) %>%
+  add_osm_feature(key = "shop", value = "mall") %>%
+  osmdata_sf() %>% .$osm_points
+mtxsh1 <- st_distance(x=Bogota_DC,y=shop1)
+tdist_sh1 <- apply(mtxsh1,1,min)
+
+shop2 <- opq(bbox = getbb("Bogotá Distrito Capital - Municipio")) %>%
+  add_osm_feature(key = "shop", value = "supermarket") %>%
+  osmdata_sf() %>% .$osm_points
+mtxsh2 <- st_distance(x=Bogota_DC,y=shop2)
+tdist_sh2 <- apply(mtxsh2,1,min)
+
+tdist_sh <- list()
+for (n in 1:37985){
+  tdist_sh[n] = min(tdist_sh1[n],tdist_sh2[n])
+}
+tdist_sh <- as.data.frame(tdist_sh)
+tdist_sh <- t(tdist_sh)
+Bogota_DC$dist_shop <- tdist_sh
+
+shop1 <- opq(bbox = getbb("Medellín Colombia")) %>%
+  add_osm_feature(key = "shop", value = c("mall","supermarket")) %>%
+  osmdata_sf() %>% .$osm_points
+mtxsh1 <- st_distance(x=Medellin_D,y=shop1)
+Medellin_D$dist_shop <- apply(mtxsh1,1,min)
+
+shop1 <- opq(bbox = getbb("Cali Colombia")) %>%
+  add_osm_feature(key = "shop", value = c("mall","supermarket")) %>%
+  osmdata_sf() %>% .$osm_points
+mtxsh1 <- st_distance(x=Cali_D,y=shop1)
+Cali_D$dist_shop <- apply(mtxsh1,1,min)
+
+# 3.3.7 Bar
+
+bar <- opq(bbox = getbb("Bogotá Distrito Capital - Municipio")) %>%
+  add_osm_feature(key = "amenity", value = "bar") %>%
+  osmdata_sf() %>% .$osm_points
+mtxbr <- st_distance(x=Bogota_DC,y=bar)
+Bogota_DC$dist_bar <- apply(mtxbr,1,min)
+
+bar <- opq(bbox = getbb("Medellín Colombia")) %>%
+  add_osm_feature(key = "amenity", value = "bar") %>%
+  osmdata_sf() %>% .$osm_points
+mtxbr <- st_distance(x=Medellin_D,y=bar)
+Medellin_D$dist_bar <- apply(mtxbr,1,min)
+
+bar <- opq(bbox = getbb("Cali Colombia")) %>%
+  add_osm_feature(key = "amenity", value = "bar") %>%
+  osmdata_sf() %>% .$osm_points
+mtxbr <- st_distance(x=Cali_D,y=bar)
+Cali_D$dist_bar <- apply(mtxbr,1,min)
+
+# 3.3.8 University
+
+univ <- opq(bbox = getbb("Bogotá Distrito Capital - Municipio")) %>%
+  add_osm_feature(key = "amenity", value = "university") %>%
+  osmdata_sf() %>% .$osm_points
+mtxun <- st_distance(x=Bogota_DC,y=univ)
+Bogota_DC$dist_univ <- apply(mtxun,1,min)
+
+univ <- opq(bbox = getbb("Medellín Colombia")) %>%
+  add_osm_feature(key = "amenity", value = "university") %>%
+  osmdata_sf() %>% .$osm_points
+mtxun <- st_distance(x=Medellin_D,y=univ)
+Medellin_D$dist_univ <- apply(mtxun,1,min)
+
+univ <- opq(bbox = getbb("Cali Colombia")) %>%
+  add_osm_feature(key = "amenity", value = "university") %>%
+  osmdata_sf() %>% .$osm_points
+mtxun <- st_distance(x=Cali_D,y=univ)
+Cali_D$dist_univ <- apply(mtxun,1,min)
+
+# 3.3.9 Restaurants
+
+rest <- opq(bbox = getbb("Bogotá Distrito Capital - Municipio")) %>%
+  add_osm_feature(key = "amenity", value = "restaurant") %>%
+  osmdata_sf() %>% .$osm_points
+mtxrs <- st_nearest_feature(x=Bogota_DC,y=rest)
+tdist_rs <- list()
+for (n in 1:37985){
+  tdist_rs[n] = st_distance(Bogota_DC$geometry[n],rest[mtxrs[n],88])
+}
+tdist_rs <- as.data.frame(tdist_rs)
+tdist_rs <- t(tdist_rs)
+Bogota_DC$dist_rest <- tdist_rs
+
+rest <- opq(bbox = getbb("Medellín Colombia")) %>%
+  add_osm_feature(key = "amenity", value = "restaurant") %>%
+  osmdata_sf() %>% .$osm_points
+mtxrs <- st_distance(x=Medellin_D,y=rest)
+Medellin_D$dist_rest <- apply(mtxrs,1,min)
+
+rest <- opq(bbox = getbb("Cali Colombia")) %>%
+  add_osm_feature(key = "amenity", value = "restaurant") %>%
+  osmdata_sf() %>% .$osm_points
+mtxrs <- st_distance(x=Cali_D,y=rest)
+Cali_D$dist_rest <- apply(mtxrs,1,min)
+
+# 3.3.9 Schools
+
+scho <- opq(bbox = getbb("Bogotá Distrito Capital - Municipio")) %>%
+  add_osm_feature(key = "amenity", value = "school") %>%
+  osmdata_sf() %>% .$osm_points
+mtxsch <- st_nearest_feature(x=Bogota_DC,y=scho)
+tdist_sch <- list()
+for (n in 1:37985){
+  tdist_sch[n] = st_distance(Bogota_DC$geometry[n],scho[mtxsch[n],48])
+}
+tdist_sch <- as.data.frame(tdist_sch)
+tdist_sch <- t(tdist_sch)
+Bogota_DC$dist_scho <- tdist_sch
+
+scho <- opq(bbox = getbb("Medellín Colombia")) %>%
+  add_osm_feature(key = "amenity", value = "school") %>%
+  osmdata_sf() %>% .$osm_points
+mtxsch <- st_distance(x=Medellin_D,y=scho)
+Medellin_D$dist_scho <- apply(mtxsch,1,min)
+
+scho <- opq(bbox = getbb("Cali Colombia")) %>%
+  add_osm_feature(key = "amenity", value = "school") %>%
+  osmdata_sf() %>% .$osm_points
+mtxsch <- st_distance(x=Cali_D,y=scho)
+Cali_D$dist_scho <- apply(mtxsch,1,min)
 
 
 
@@ -40,4 +326,20 @@ geoprop2 <- st_buffer(x=geoprop1,dist=2000)
 
 
 
+# Oldcode
+nm = c("Bogotá Distrito Capital - Municipio","Medellín Colombia","Cali Colombia")
 
+for (vl in 1:3){
+  vps[[vl]] <- opq(bbox = getbb(nm[vl])) %>%
+    add_osm_feature(key = "highway", value=c("trunk","primary","secondary","tertiary")) %>%
+    osmdata_sf() %>% .$osm_lines
+  
+  wat[[vl]] <- opq(bbox = getbb(nm[vl])) %>%
+    add_osm_feature(key = "waterway", value=c("river","stream","canal")) %>%
+    osmdata_sf() %>% .$osm_lines
+  
+  park[[vl]] <- opq(bbox = getbb(nm[vl])) %>%
+    add_osm_feature(key = "leisure", value = "park") %>%
+    osmdata_sf() %>% .$osm_polygons
+  
+}
---
+++ prepare_censo.R
@@ -0,0 +1,149 @@
+
+## load packages
+require(pacman) 
+p_load(tidyverse,rio)
+
+## censo data
+browseURL("https://microdatos.dane.gov.co//catalog/643/get_microdata")
+
+##=== variables ===##
+
+## COD_DANE_ANM: Codigo DANE de manzana
+## UA_CLASE: ID
+## COD_ENCUESTAS: ID de encuesta
+## U_VIVIENDA: ID de vivienda
+## H_NRO_CUARTOS: Número de cuartos en total
+## HA_TOT_PER: Total personas en el hogar
+## V_TOT_HOG: Total de hogares en la vivienda
+## VA1_ESTRATO: Estrato de la vivienda (según servicio de energía)
+
+
+##=== Bogota, load data ===##
+
+## unzip file
+unzip(zipfile="Censo/11_BOGOTA_CSV.zip" , exdir="Censo/." , overwrite=T) 
+
+## data manzanas
+mgn_bog <- import("Censo/CNPV2018_MGN_A2_11.CSV")
+colnames(mgn_bog)
+mgn_bog <- mgn_bog %>% select(COD_DANE_ANM,UA_CLASE,COD_ENCUESTAS,U_VIVIENDA)
+
+## data hogar
+hog_bog <- import("Censo/CNPV2018_2HOG_A2_11.CSV")
+colnames(hog_bog)
+hog_bog <- hog_bog %>% select(UA_CLASE,COD_ENCUESTAS,U_VIVIENDA,H_NROHOG,H_NRO_CUARTOS,HA_TOT_PER)
+
+## data vivienda
+viv_bog <- import("Censo/CNPV2018_1VIV_A2_11.CSV") 
+colnames(viv_bog)
+viv_bog <- viv_bog %>% select(COD_ENCUESTAS,UA_CLASE,U_VIVIENDA,V_TOT_HOG,VA1_ESTRATO)
+
+## join hogar-vivienda
+viv_hog_bog <- left_join(hog_bog,viv_bog,by=c("COD_ENCUESTAS","U_VIVIENDA","UA_CLASE"))
+
+## joing mnz-hogar-vivienda
+viv_hog_mgn_bog <- left_join(viv_hog_bog,mgn_bog,by=c("UA_CLASE","COD_ENCUESTAS","U_VIVIENDA"))
+
+##=== collapse data ===##
+db_bog <- viv_hog_mgn_bog %>%
+      group_by(COD_DANE_ANM) %>% 
+      summarise(med_H_NRO_CUARTOS=median(H_NRO_CUARTOS,na.rm=T), 
+                sum_HA_TOT_PER=sum(HA_TOT_PER,na.rm=T), 
+                med_V_TOT_HOG=median(V_TOT_HOG,na.rm=T),
+                med_VA1_ESTRATO=median(VA1_ESTRATO,na.rm=T))
+
+## export data
+#export(db_bog,"mnzbog_censo_2018.rds")
+
+
+
+##=== Medellin, load data ===##
+
+## unzip file
+unzip(zipfile="Censo/05_ANTIOQUIA_CSV.zip" , exdir="Censo/." , overwrite=T) 
+
+## data manzanas
+mgn_med <- import("Censo/CNPV2018_MGN_A2_05.CSV")
+colnames(mgn_med)
+mgn_med <- filter(mgn_med, U_MPIO==1)
+mgn_med <- mgn_med %>% select(COD_DANE_ANM,UA_CLASE,COD_ENCUESTAS,U_VIVIENDA)
+
+## data hogar
+hog_med <- import("Censo/CNPV2018_2HOG_A2_05.CSV")
+colnames(hog_med)
+hog_med <- filter(hog_med, U_MPIO==1)
+hog_med <- hog_med %>% select(UA_CLASE,COD_ENCUESTAS,U_VIVIENDA,H_NROHOG,H_NRO_CUARTOS,HA_TOT_PER)
+
+## data vivienda
+viv_med <- import("Censo/CNPV2018_1VIV_A2_05.CSV") 
+colnames(viv_med)
+viv_med <- filter(viv_med, U_MPIO==1)
+viv_med <- viv_med %>% select(COD_ENCUESTAS,UA_CLASE,U_VIVIENDA,V_TOT_HOG,VA1_ESTRATO)
+
+## join hogar-vivienda
+viv_hog_med <- left_join(hog_med,viv_med,by=c("COD_ENCUESTAS","U_VIVIENDA","UA_CLASE"))
+
+## joing mnz-hogar-vivienda
+viv_hog_mgn_med <- left_join(viv_hog_med,mgn_med,by=c("UA_CLASE","COD_ENCUESTAS","U_VIVIENDA"))
+
+##=== collapse data ===##
+db_med <- viv_hog_mgn_med %>%
+  group_by(COD_DANE_ANM) %>% 
+  summarise(med_H_NRO_CUARTOS=median(H_NRO_CUARTOS,na.rm=T), 
+            sum_HA_TOT_PER=sum(HA_TOT_PER,na.rm=T), 
+            med_V_TOT_HOG=median(V_TOT_HOG,na.rm=T),
+            med_VA1_ESTRATO=median(VA1_ESTRATO,na.rm=T))
+
+## export data
+#export(db_med,"mnzmed_censo_2018.rds")
+
+
+
+##=== Cali, load data ===##
+
+## unzip file
+unzip(zipfile="Censo/76_VALLEDELCAUCA_CSV.zip" , exdir="Censo/." , overwrite=T) 
+
+## data manzanas
+mgn_cal <- import("Censo/CNPV2018_MGN_A2_76.CSV")
+colnames(mgn_cal)
+mgn_cal <- filter(mgn_cal, U_MPIO==1)
+mgn_cal <- mgn_cal %>% select(COD_DANE_ANM,UA_CLASE,COD_ENCUESTAS,U_VIVIENDA)
+
+## data hogar
+hog_cal <- import("Censo/CNPV2018_2HOG_A2_76.CSV")
+colnames(hog_cal)
+hog_cal <- filter(hog_cal, U_MPIO==1)
+hog_cal <- hog_cal %>% select(UA_CLASE,COD_ENCUESTAS,U_VIVIENDA,H_NROHOG,H_NRO_CUARTOS,HA_TOT_PER)
+
+## data vivienda
+viv_cal <- import("Censo/CNPV2018_1VIV_A2_76.CSV") 
+colnames(viv_cal)
+viv_cal <- filter(viv_cal, U_MPIO==1)
+viv_cal <- viv_cal %>% select(COD_ENCUESTAS,UA_CLASE,U_VIVIENDA,V_TOT_HOG,VA1_ESTRATO)
+
+## join hogar-vivienda
+viv_hog_cal <- left_join(hog_cal,viv_cal,by=c("COD_ENCUESTAS","U_VIVIENDA","UA_CLASE"))
+
+## joing mnz-hogar-vivienda
+viv_hog_mgn_cal <- left_join(viv_hog_cal,mgn_cal,by=c("UA_CLASE","COD_ENCUESTAS","U_VIVIENDA"))
+
+##=== collapse data ===##
+db_cal <- viv_hog_mgn_cal %>%
+  group_by(COD_DANE_ANM) %>% 
+  summarise(med_H_NRO_CUARTOS=median(H_NRO_CUARTOS,na.rm=T), 
+            sum_HA_TOT_PER=sum(HA_TOT_PER,na.rm=T), 
+            med_V_TOT_HOG=median(V_TOT_HOG,na.rm=T),
+            med_VA1_ESTRATO=median(VA1_ESTRATO,na.rm=T))
+
+## export data
+#export(db_cal,"mnzcal_censo_2018.rds")
+
+
+##=== MNZ Data ===##
+
+mnz_data <-rbind(db_bog,db_med,db_cal)
+
+export(mnz_data,"mnzdata_censo_2018.rds")
+
+
